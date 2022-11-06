522f034bca71423922f49d9a493162d309af52c7
Subhro Acharjee <88422396+subhroacharjee@users.noreply.github.com>
2022-11-07 00:06:02 +0530

    adding get message list

--- src/controllers/Message.controller.ts
+++ src/controllers/Message.controller.ts
@@ -7,9 +7,10 @@ import MessageModel from '../models/MessageModel';
 
 import * as Events from '../common/Constants/Events';
 import { ControllerReturnType } from '../interfaces/Responses/Controller';
-import { Message, MessageResponseObject } from '../interfaces/Responses/Message';
+import { MessageResponseObject } from '../interfaces/Responses/Message';
 import { IUserShort } from '../interfaces/Model/User';
 import _ from 'lodash';
+import { Default500Response } from '../common/Constants/DefaultErrStatus';
 
 
 export const JoinChatRoomsController = async(currentUser: CurrentUser, socket: Socket) => {
@@ -68,48 +69,90 @@ export const GetMessages = async(currentUser: CurrentUser, roomKeys?: string[], 
 	// 3. if roomKey is undefined or empty assign all the values.
 	// 4. find all the messages
 	// 5. group by the key using lodash or if possible mongoose itsefl
-	let validatedRoomKeys: string[] = [];
+	try {
+		let validatedRoomKeys: string[] = [];
 
-	const connKeyList = (await ConnectionModel.find({
-		u1_id: currentUser._id
-	}).select('key').exec());
+		const connKeyList = (await ConnectionModel.find({
+			u1_id: currentUser._id
+		}).select('key').exec());
 
-	if (roomKeys && roomKeys.length > 0) {
-		// if the roomkey is valid or not.
-		validatedRoomKeys = _.filter(roomKeys, (roomKey) => _.findIndex(connKeyList, (doc) => doc.key === roomKey) !== -1);
-	} else {
-		validatedRoomKeys = _.map(connKeyList, (doc) => doc.key);
+		if (roomKeys && roomKeys.length > 0) {
+			// if the roomkey is valid or not.
+			validatedRoomKeys = _.filter(roomKeys, (roomKey) => _.findIndex(connKeyList, (doc) => doc.key === roomKey) !== -1);
+		} else {
+			validatedRoomKeys = _.map(connKeyList, (doc) => doc.key);
+		}
+
+
+		const unGroupedMessageList = await MessageModel.find()
+			.where('connectionKey')
+			.in(validatedRoomKeys)
+			.populate({
+				path: 'sender',
+				select: '_id username'
+			})
+			.select('sender message connectionKey created_at')
+			.sort('-created_at')
+			.limit(1000)
+			.skip(skip || 0)
+			.exec();
+
+		const groupedMessageObject =_.groupBy(unGroupedMessageList, 'connectionKey');
+		
+		const messages: MessageResponseObject = {} as MessageResponseObject;
+
+
+		_.forEach(groupedMessageObject, (value, key) => {
+			messages[key] = _.map(value, (doc) => ({
+				sender: <IUserShort>doc.sender,
+				message: doc.message.trim(),
+				created_at: doc.created_at
+			}));
+		});
+
+		return {
+			data: messages,
+			error: null
+		};
+	} catch (error) {
+		return Default500Response;
+	}
+};
+
+export const GetAllConnectionWithMessage = async(currentUser: CurrentUser): Promise<ControllerReturnType<IUserShort[]>> => {
+	// 1. get all the message room keys from message table, by using currentUser as sender
+	// 2. get all the u1 from connection
+
+	try {
+		const keys = await MessageModel.find({
+			sender: currentUser._id
+		})
+			.select('connectionKey')
+			.distinct('connectionKey')
+			.exec();
+		
+		const connectionU1List = await ConnectionModel.find({
+			u2_id: currentUser._id
+		})
+			.where('key')
+			.in(keys)
+			.populate({
+				path: 'u1_id',
+				select: '_id username'
+			})
+			.exec();
+		
+		const userList: IUserShort[] = connectionU1List.map(conn => ({
+			_id: conn.u1_id._id,
+			username: conn.u1_id.username
+		}));
+	
+		return {
+			data: userList,
+			error: null
+		};
+	} catch (error) {
+		return Default500Response;
 	}
 
-
-	const unGroupedMessageList = await MessageModel.find()
-		.where('connectionKey')
-		.in(validatedRoomKeys)
-		.populate({
-			path: 'sender',
-			select: '_id username'
-		})
-		.select('sender message connectionKey created_at')
-		.sort('-created_at')
-		.limit(1000)
-		.skip(skip || 0)
-		.exec();
-
-	const groupedMessageObject =_.groupBy(unGroupedMessageList, 'connectionKey');
-	
-	const messages: MessageResponseObject = {} as MessageResponseObject;
-
-
-	_.forEach(groupedMessageObject, (value, key) => {
-		messages[key] = _.map(value, (doc) => ({
-			sender: <IUserShort>doc.sender,
-			message: doc.message.trim(),
-			created_at: doc.created_at
-		}));
-	});
-
-	return {
-		data: messages,
-		error: null
-	};
 };
--- src/routes/message.route.ts
+++ src/routes/message.route.ts
@@ -1,9 +1,41 @@
 import express, { Router } from 'express';
 import Logger from '../common/log';
-import { GetMessages } from '../controllers/Message.controller';
+import { GetAllConnectionWithMessage, GetMessages } from '../controllers/Message.controller';
 import { CurrentUser } from '../interfaces/Responses/Auth';
 
 const MessageRouter = Router();
+
+MessageRouter.get('/userList', (req: express.Request, res: express.Response) => {
+	const currentUser = req.headers.currentUser;
+	if (currentUser && typeof currentUser === 'string') {
+		GetAllConnectionWithMessage(JSON.parse(currentUser))
+			.then(response => {
+				if (response.error !== null) {
+					const status = response.error._xstatus ? parseInt(response.error._xstatus[0]) : 400;
+					delete response.error._xstatus;
+					res.status(status).json({
+						status: 'failed',
+						error: response.error
+					});
+					return;
+				}
+	
+				res.json({
+					status: 'success',
+					data: response.data
+				});
+			})
+			.catch(err => {
+				Logger.error(JSON.stringify(err));
+				res.status(500).json({
+					success: 'failed',
+					error: 'Something went wrong'
+				});
+			});
+	} else {
+		res.status(403).send();
+	}
+});
 
 MessageRouter.get('/', (req: express.Request, res: express.Response) => {
 	let roomKeys:string[];
